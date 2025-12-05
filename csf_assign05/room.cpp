3b08a8d5d2a2a0c227a22fd1304075b2a50d9441
Senuka Abeysinghe <senukaabeysinghe@Senukas-MacBook-Pro.local>
2025-12-05 18:12:38 -0500

    added room func changes

--- csf_assign05/room.cpp
+++ csf_assign05/room.cpp
@@ -6,29 +6,38 @@
 
 Room::Room(const std::string &room_name)
   : room_name(room_name) {
-  pthread_mutex_init(&m_lock, nullptr); // init mutex
+  pthread_mutex_init(&lock, nullptr); // init mutex
 }
 
 Room::~Room() {
-  pthread_mutex_destroy(&m_lock); // destroy mutex
+  pthread_mutex_destroy(&lock); // destroy mutex
 }
 
 void Room::add_member(User *user) {
-  Guard g(m_lock);
+  //Guard g(lock);
+  pthread_mutex_lock(&lock);
   members.insert(user); // add user to room
+  pthread_mutex_unlock(&lock);
 }
 
 void Room::remove_member(User *user) {
   // TODO: remove User from the room
-  Guard g(m_lock);
+  /*Guard g(m_lock);
+  members.erase(user);*/
+  pthread_mutex_lock(&lock);
   members.erase(user);
+  pthread_mutex_unlock(&lock);
 }
 
 void Room::broadcast_message(const std::string &sender_username, const std::string &message_text) {
   // TODO: send a message to every (receiver) User in the room
-  Guard g(m_lock);
-  for (auto u : members) {
-    Message *msg = new Message(TAG_DELIVERY, sender_username + ": " + message_text);
-    u->mqueue.enqueue(msg); // enqueue for each receiver
+  pthread_mutex_lock(&lock);
+
+  //Guard g(lock);
+  for (User* user_in_members : members) {
+    Message *msg = new Message(TAG_DELIVERY, room_name + ": " + sender_username + ": " + message_text);
+    user_in_members->mqueue.enqueue(msg); // enqueue for each receiver
   }
+
+  pthread_mutex_unlock(&lock);
 }
